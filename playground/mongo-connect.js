49bc6948cd8af2848672d9f1d2f3630c096b8b89
shivgaurav27 <shivgaurav27@gmail.com>
2020-12-20 20:09:56 +0530

    initial commit mongo db

---
+++ playground/mongo-connect.js
@@ -0,0 +1,22 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect("mongodb://localhost: 27017/TodoApp", (err, client) => {
+  if (err) {
+    return console.log("Unable to connet to MongoDb Sever!!");
+  }
+  console.log("Successfully connected MongoDb Server!!");
+  const db = client.db("TodoApp");
+  db.collection("Todos").insertOne(
+    {
+      text: "Something to do-1",
+      completed: false,
+    },
+    (err, result) => {
+      if (err) {
+        return console.log("Unable to insert todo", err);
+      }
+      console.log(JSON.stringify(result.ops, undefined, true));
+    }
+  );
+  client.close();
+});
