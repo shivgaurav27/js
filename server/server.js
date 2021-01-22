4293976ed25d2173579e3e8f3d65c9841bc52cd9
shivgaurav27 <shivgaurav27@gmail.com>
2021-01-23 00:19:55 +0530

    first api build not rest api only api to insert todos

--- server/server.js
+++ server/server.js
@@ -1,3 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { user } = require("./models/user");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.post("/todos", (req, res) => {
+  //   console.log(req.body);
+  var todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save().then(
+    (doc) => {
+      res.send(doc);
+    },
+    (err) => {
+      res.status(400).send(err);
+    }
+  );
+});
+
+// GET
+
+app.listen(3000, () => {
+  console.log("startted on port 3000");
+});
