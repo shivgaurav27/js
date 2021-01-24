3b95d2fbdecd824be59a0b52bce0a8dd2e10a919
shivgaurav27 <shivgaurav27@gmail.com>
2021-01-25 01:24:45 +0530

    sending html pages in response

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 const adminRoutes = require("./routes/admin");
@@ -12,7 +13,8 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).send("<h1> Page Not Found </h1>");
+  //   res.status(404).send("<h1> Page Not Found </h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "PageNotFOund.html"));
 });
 
 const server = http.createServer(app);
