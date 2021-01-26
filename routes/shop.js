1523d4603b212efc8684a5878cfbdf3bf57d2a22
shivgaurav27 <shivgaurav27@gmail.com>
2021-01-26 21:15:26 +0530

    use template engine pug

--- routes/shop.js
+++ routes/shop.js
@@ -9,9 +9,10 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   console.log(adminData.products);
+  const products = adminData.products;
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   // use template engine pug below to render shop page
-  res.render("shop");
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
