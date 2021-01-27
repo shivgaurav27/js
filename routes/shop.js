964343e33d32013e8a74cdd8409900fa81a7436d
shivgaurav27 <shivgaurav27@gmail.com>
2021-01-27 21:40:00 +0530

    added dynamic classes

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 const adminData = require("./admin");
 
 const router = express.Router();
@@ -12,7 +12,7 @@ router.get("/", (req, res, next) => {
   const products = adminData.products;
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   // use template engine pug below to render shop page
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", { prods: products, docTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
