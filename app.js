75fcb4dcc6fe8a7a53b0a168acf0615e0e62d182
shivgaurav27 <shivgaurav27@gmail.com>
2021-01-27 23:57:52 +0530

    ejs implementation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
   res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
