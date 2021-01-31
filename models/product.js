3fb504840d4c3f9caa08a9c2856fb48b340c5e7c
shivgaurav27 <shivgaurav27@gmail.com>
2021-01-31 22:12:43 +0530

    aded call back function and  read data from file

--- models/product.js
+++ models/product.js
@@ -1,14 +1,41 @@
-const products = [];
+const { error } = require("console");
+const fs = require("fs");
+const path = require("path");
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "product.json"
+    );
+    fs.readFile(p, (error, fileContent) => {
+      let products = [];
+      if (!error) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "product.json"
+    );
+    fs.readFile(p, (error, fileContent) => {
+      if (error) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
