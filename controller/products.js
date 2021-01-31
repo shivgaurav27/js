abd0ece10f396f27fcba306b29d32243b6a0b6bc
shivgaurav27 <shivgaurav27@gmail.com>
2021-01-31 07:49:43 +0530

    make folders in view

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -18,7 +18,7 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   const products = Product.fetchAll();
 
-  res.render("shop", {
+  res.render("shop/product-list", {
     prods: products,
     pageTitle: "Shop",
     path: "/",
