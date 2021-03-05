cfef5428222c707279280aa8f3811aca51c50da3
shivgaurav27 <shivgaurav27@gmail.com>
2021-03-06 02:59:41 +0530

    mvc folder 8 added zip files

--- models/product.js
+++ models/product.js
@@ -1,30 +1,35 @@
-const { error } = require("console");
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "product.json"
+  'data',
+  'products.json'
 );
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (error, fileContent) => {
-    if (error) {
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
       cb([]);
     } else {
       cb(JSON.parse(fileContent));
     }
   });
 };
+
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (error) => {
-        console.log(error);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
   }
