8d7c450454be9a292ecd953ae862be3930438c35
shivgaurav27 <shivgaurav27@gmail.com>
2021-03-07 18:04:00 +0530

    find all method by sequalize in js

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,10 +2,10 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/product-list", {
-        prods: rows,
+        prods: products,
         pageTitle: "All Products",
         path: "/products",
       });
@@ -27,14 +27,16 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
         path: "/",
       });
     })
+    .catch((err) => console.log(err))
+    .then(([rows, fieldData]) => {})
     .catch((err) => console.log(err));
 };
 
