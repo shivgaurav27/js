0a3a43d8301df923c1607afcb9a24bf0222ec67b
shivgaurav27 <shivgaurav27@gmail.com>
2021-03-14 17:14:16 +0530

    sequalize added

--- controllers/shop.js
+++ controllers/shop.js
@@ -152,10 +152,16 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
