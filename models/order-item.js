fd387db018726caafc68fddc8e4026a232e4aa71
shivgaurav27 <shivgaurav27@gmail.com>
2021-03-14 14:51:26 +0530

    order and order item models added

---
+++ models/order-item.js
@@ -0,0 +1,14 @@
+const Sequalize = require("sequelize");
+const sequelize = require("../util/database");
+
+const OrderItem = sequelize.define("orderItem", {
+  id: {
+    type: Sequalize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  quantity: Sequalize.INTEGER,
+});
+
+module.exports = OrderItem;
