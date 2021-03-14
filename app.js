297d0c8c0918d33d73130ee7c9f0b70031f42be0
shivgaurav27 <shivgaurav27@gmail.com>
2021-03-14 12:57:31 +0530

    associations added with logged in user query

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require("./util/database");
 
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cartItem");
 
 const app = express();
 
@@ -20,6 +22,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -27,11 +38,30 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
+    return User.findByPk(1);
     // console.log(result);
+    app.listen(3000);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "shiv", email: "abc@abc.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
