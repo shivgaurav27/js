b80ff1fa38c7e029c995e51316b6d5f19b6dccc3
shivgaurav27 <shivgaurav27@gmail.com>
2021-03-21 17:22:14 +0530

    fixier

--- util/database.js
+++ util/database.js
@@ -1,18 +1,18 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-const mongoConnect = callback => {
+const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    "mongodb+srv://shiv:Shiv@12345@tododb.wml9s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   )
-    .then(client => {
-      console.log('Connected!');
+    .then((client) => {
+      console.log("Connected!");
       _db = client.db();
       callback();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       throw err;
     });
@@ -22,7 +22,7 @@ const getDb = () => {
   if (_db) {
     return _db;
   }
-  throw 'No database found!';
+  throw "No database found!";
 };
 
 exports.mongoConnect = mongoConnect;
